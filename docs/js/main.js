56fe08ee0f2e857fc58006057e63ca90824704d5
Anton Machkasov <anton.machkasov@gmail.com>
2020-12-12 22:33:16 +0200

    Init commit with page stub

---
+++ docs/js/main.js
@@ -0,0 +1,12 @@
+
+(function($) {
+
+	"use strict";
+
+   $(window).load(function() {
+    	$("#loader").fadeOut("slow", function(){
+        $("#preloader").delay(300).fadeOut("slow");
+      });
+  	})
+
+})(jQuery);
